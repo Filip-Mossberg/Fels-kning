c2796b3b7d36d78b3b2fcc201039cf8710ce7c45
Filip <Filip@FILIPS>
2022-10-01 19:26:02 +0200

    Add project files.

---
+++ Felsökning/Program.cs
@@ -0,0 +1,82 @@
+﻿// Filip Mossberg
+
+using System;
+
+namespace Felsökning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Case 1
+            /*
+            int number = 4;
+
+            if (number > 3)
+            {
+                Console.WriteLine("Talet är större än tre"); // Closing the row by adding ;
+            }
+            else if (number < 3) // Ett mellanslag vid else if 
+            {
+                Console.WriteLine("Talet är mindre än tre");
+            }
+            
+            
+            // Case 2 
+            for (int i = 1; i <= 100; i++) // Added = so it runs while the number is = to 100 not just lower then
+            {
+                Console.WriteLine(i);
+            }
+            
+            
+            // Case 3 
+            for (int i = 1; i <= 5; i++) // Added ++ after the last i to make it count how many times the loop has been done
+            {
+                for (int j = 1; j <= i; j++) // Added ++ after the last j to make it count how many times the loop has been done
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+            
+            
+            // Case 4
+            int i = 1;
+            while (i <= 5)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(i); // Placed the variable i in Console.Write()
+                }
+
+                Console.WriteLine();      
+                i++;
+            }
+            
+            
+            // Case 5
+            int i = 1;
+
+            switch (i)
+            {
+                // Removed the first case 
+                case 2:
+                    Console.WriteLine("Two");
+                    break;
+                default:
+                    Console.WriteLine("Other");
+                    break;
+            }
+            
+
+            // Case 6
+            int i = 5; // Changed the value of i to 5 instead of 10 to make the if run
+
+            if (i == 5) // Had to add another = symbol
+            {
+                Console.WriteLine("i är 5");
+            }
+            */
+        }
+    }
+}
